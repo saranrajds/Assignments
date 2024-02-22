d395c1667480964301e6020d5545ec0bf244337b
saranrajds <159820053+saranrajds@users.noreply.github.com>
2024-02-22 10:36:56 +0530

    Update Program_01

--- Assignment_02/Program_01.java
+++ Assignment_02/Program_01.java
@@ -5,9 +5,10 @@ public class Program_01 {
 	public static void main(String[] args) {
 //		1. Write a program to convert Celsius to Fahrenheit using appropriate datatypes and literal conventions
 		
-		double celsius = 25.0;
-		double fahrenheit = (9/5 * celsius) + 32;
-	    System.out.println("Celsius : "+ celsius + " Fahrenheit : " + fahrenheit);
+		double celsius = 30;
+		double temp = (double)9/5;
+		double fahrenheit = ( temp * celsius) + 32;
+	    	System.out.println("Celsius : "+ celsius + " Fahrenheit : " + fahrenheit);
 	    
 	}
 }
